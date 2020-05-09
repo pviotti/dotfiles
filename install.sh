1a3e0cac569be5d757f165e53f56e8c2109fd94c
Paolo <pviotti@users.noreply.github.com>
2020-05-09 23:57:55 +0100

    add inputrc (for vi mode readline)

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ rm -f ~/.zshrc
 ln -fs "$pwd/zshrc" "$HOME/.zshrc"
 ln -fs "$pwd/tmux.conf" "$HOME/.tmux.conf"
 ln -fs "$pwd/gitconfig" "$HOME/.gitconfig"
+ln -fs "$pwd/inputrc" "$HOME/.inputrc"
 ln -fs "$pwd/pam_environment" "$HOME/.pam_environment"
 ln -fs "$pwd/user-dirs.dirs" "$HOME/.config/user-dirs.dirs"
 
