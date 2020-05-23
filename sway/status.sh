f8c923ffb37858c4163619072ea4c0d3f8a4d62c
Paolo <pviotti@users.noreply.github.com>
2020-05-23 12:01:58 +0100

    status.sh: ms keyboard

--- sway/status.sh
+++ sway/status.sh
@@ -35,7 +35,7 @@ audio_volume=$(amixer -M get Master |
 
 # Keyboard layout
 current_kb_layout=$(swaymsg -p -t get_inputs |
-    grep "Dell USB Entry Keyboard$" -A5 -m1 | tr -d "[:space:]" | cut -d":" -f9)
+    grep "Microsoft Wired Keyboard 600$" -A5 -m1 | tr -d "[:space:]" | cut -d":" -f9)
 case $current_kb_layout in
     "Italian") keyboard_layout="IT⌨" ;;
     "Irish") keyboard_layout="IE⌨" ;;
