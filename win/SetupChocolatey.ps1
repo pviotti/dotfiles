0529c2a29a2041d5b438b48541c1a1eb8de00103
Paolo Viotti <pviotti@microsoft.com>
2020-07-21 14:06:30 +0100

    add SetupChocolatey script

---
+++ win/SetupChocolatey.ps1
@@ -0,0 +1,58 @@
+<#
+.SYNOPSIS
+SetupChocolatey.
+
+.DESCRIPTION
+Simple script to setup Chocolatey for a Windows development workstation.
+Inspired by https://github.com/adityastic/Windows-Config
+#>
+
+$BasicPackages = -split "git vscode vlc powertoys microsoft-windows-terminal postman python telegram poshgit powershell-core lockhunter ilspy gimp nextcloud-client nmap notepad3"
+
+function InstallChocolatey() {
+    Write-Host "Installing Chocolatey"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
+}
+
+function InstallBasicPackages(){
+    Write-Host "Chocolatey - Install basic packages"
+    choco install -y $BasicPackages
+}
+
+function ScheduleUpgradeJob() {
+    Import-Module PSScheduledJob
+    Write-Host "Chocolatey - Create background task to upgrade all packages"
+    $ScheduledJob = @{
+        Name = "Chocolatey upgrade"
+        ScriptBlock = {choco upgrade all -y}
+        Trigger = New-JobTrigger -Weekly -DaysOfWeek Monday, Wednesday, Friday -At "15:00"
+        ScheduledJobOption = New-ScheduledJobOption -RunElevated -MultipleInstancePolicy StopExisting -RequireNetwork
+    }
+    Register-ScheduledJob @ScheduledJob
+}
+
+function Show-Menu {
+    param (
+        [string]$Title = 'Chocolatey Setup'
+    )
+    Clear-Host
+    Write-Host "================ $Title ================"
+    Write-Host "1: Install Chocolatey."
+    Write-Host "2: Install basic packages."
+    Write-Host "3: Create background task to upgrade all packages."
+    Write-Host "q: Quit."
+}
+
+do
+ {
+    Show-Menu
+    $selection = Read-Host "> "
+    switch ($selection)
+    {
+        '1' { InstallChocolatey } 
+        '2' { InstallBasicPackages } 
+        '3' { ScheduleUpgradeJob }
+    }
+    pause
+ }
+ until ($selection -eq 'q')
