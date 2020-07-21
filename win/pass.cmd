ebea8ca207318d9c217bcb1554c356e45435db75
Paolo Viotti <pviotti@microsoft.com>
2020-07-21 14:06:03 +0100

    change veracrypt volume path

--- win/pass.cmd
+++ win/pass.cmd
@@ -1,4 +1,4 @@
 @echo off
-"C:\Program Files\VeraCrypt\VeraCrypt.exe" /v C:%HOMEPATH%\Nextcloud\Documents\tc_volume /l A /q
+"C:\Program Files\VeraCrypt\VeraCrypt.exe" /v C:%HOMEPATH%\vshed\docs\tc_volume /l A /q
 type A:\pwd\*%1*
 "C:\Program Files\VeraCrypt\VeraCrypt.exe" /d /q
