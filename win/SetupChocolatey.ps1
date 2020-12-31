8571cb9b8acb8969e0f16fa96b3537099872d885
Paolo <pviotti@users.noreply.github.com>
2020-12-31 10:45:26 +0000

    chore: add wiztree processhacker paint.net & codetrack to windows tools

--- win/SetupChocolatey.ps1
+++ win/SetupChocolatey.ps1
@@ -7,7 +7,7 @@ Simple script to setup Chocolatey for a Windows development workstation.
 Inspired by https://github.com/adityastic/Windows-Config
 #>
 
-$BasicPackages = -split "git vscode vlc powertoys microsoft-windows-terminal postman python telegram poshgit powershell-core lockhunter ilspy gimp nextcloud-client nmap notepad3 neovim lf docker-desktop"
+$BasicPackages = -split "git vscode vlc powertoys microsoft-windows-terminal postman python telegram poshgit powershell-core lockhunter ilspy gimp nextcloud-client nmap notepad3 neovim lf docker-desktop wiztree processhacker paint.net codetrack"
 
 function InstallChocolatey() {
     Write-Host "Installing Chocolatey"
