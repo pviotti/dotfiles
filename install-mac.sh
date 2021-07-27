6be24193387afbb780b2aca608b7f1483d790ea5
Paolo Viotti <paolo.viotti@workday.com>
2021-07-27 22:53:35 +0100

    mac: remove useless last login welcome msg in terminal

--- install-mac.sh
+++ install-mac.sh
@@ -5,3 +5,6 @@ pwd=$(pwd)
 
 mkdir -p "$HOME/Library/KeyBindings"
 ln -fs "$pwd/mac/DefaultKeyBinding.dict" "$HOME/Library/KeyBindings/DefaultKeyBinding.dict"
+
+# remove "Last login.." initial message when opening a terminal
+touch ~/.hushlogin
